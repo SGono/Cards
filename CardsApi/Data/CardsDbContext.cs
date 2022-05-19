171be0a90bc5ec021019efb07aa1f7485804433a
SGono <stellag@cortexware.co.za>
2022-05-19 16:02:45 +0200

    Add project files.

---
+++ CardsApi/Data/CardsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CardsApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CardsApi.Data
+{
+    public class CardsDbContext : DbContext
+    {
+        public CardsDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        // Dbset
+        public DbSet<Card> Cards{ get; set; }
+    }
+}
